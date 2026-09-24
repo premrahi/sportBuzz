23931625fb253859316642ba6354a237fa914864
premrahi <premrahi234@gmail.com>
2026-09-24 12:35:49 +0530

    implemented database schemas

--- src/index.js
+++ src/index.js
@@ -9,5 +9,5 @@ app.get('/' , (req, res) => {
 })
 
 app.listen(PORT , () => {
-    console.log(`server is up!!! and running at : ${PORT}`) ;
+    console.log(`server is up!!! and running at : ${PORT}`);
 })
